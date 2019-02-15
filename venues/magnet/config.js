76384b94b3dd8985e43c5eb4242744f1e2b0cc8d
okitan <okitakunio@gmail.com>
2019-02-15 09:45:25 +0900

    :seedling: add magnet

---
+++ venues/magnet/config.js
@@ -0,0 +1,24 @@
+"use strict";
+
+module.exports = {
+  id: "4c0f742698102d7f2f32e506",
+  name: "MAGNET by SHIBUYA109",
+  subvenues: [{ id: "5af43e0f336273003909fe13", name: "MAG7" }],
+  linker: {
+    ignore: []
+  }
+};
+
+module.exports.scraper = [
+  // {
+  //   url: "URL",
+  //   venues: {
+  //     SELECTOR: {
+  //       name: { selector: "SELECTOR" },
+  //       altName: { selector: "SELECTOR" },
+  //       phone: { selector: "SELECTOR" },
+  //       level: { selector: "SELECTOR" }
+  //     }
+  //   }
+  // }
+];
