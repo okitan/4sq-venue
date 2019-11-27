e2d74484930877c54354834e6dc2ba14988744ec
okitan <okitakunio@gmail.com>
2019-11-27 16:08:10 +0900

    :bulb: push

---
+++ script/push.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -eu
+
+target=$1
+dir="venues/$target"
+
+if [ -z "$target" ] || [ ! -d "$dir" ]; then
+  echo "specify correct venue name"
+  exit 1
+fi
+
+branch="scrape-$target"
+set -x
+if [[ -n "$(git status --porcelain "$dir")" ]]; then
+  git config user.name okitan
+  git config user.email okitakunio@gmail.com
+
+  git add -A "$dir"
+  git commit -m ":innocent: scraped venue of $1"
+
+  git remote set-url origin "https://okitan:$GITHUB_TOKEN@github.com/okitan/actions-sandbox.git"
+
+  git push origin "$branch"
+fi
