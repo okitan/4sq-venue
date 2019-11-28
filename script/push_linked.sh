6d94f076bfb32abe8a1391c29b8aefc01ed9b52b
okitan <okitakunio@gmail.com>
2019-11-28 09:21:11 +0900

    :bulb: add link workflow

---
+++ script/push_linked.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -eu
+
+target=$1
+dir="venues/$target"
+
+if [ -z "$target" ] || [ ! -d "$dir" ]; then
+  echo "specify correct venue name"
+  exit 1
+fi
+
+branch="link-$target"
+set -x
+if [[ -n "$(git status --porcelain "$dir")" ]]; then
+  git config user.name okitan
+  git config user.email okitakunio@gmail.com
+
+  # TODO: when just adding scraped venues to nolinked, do commit to master directly
+  git add -A "$dir"
+  git commit -m ":innocent: scraped venue of $1"
+
+  git remote set-url origin "https://okitan:$GITHUB_TOKEN@github.com/okitan/actions-sandbox.git"
+
+  git push origin "$branch"
+fi
