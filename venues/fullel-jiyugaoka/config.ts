3493d75b886d953ae6990da2eb71dcdbf1a5f59d
okitan <okitakunio@gmail.com>
2020-04-01 21:06:39 +0900

    :bulb: make config ts

--- venues/fullel-jiyugaoka/config.ts
+++ venues/fullel-jiyugaoka/config.ts
@@ -1,16 +1,13 @@
-"use strict";
+import { Config } from "../../src/types/config";
 
-module.exports = {
+const config: Config = {
   id: "4ddee63c1fc70c691ef7fda3",
   name: "フレル・ウィズ 自由が丘",
   subvenues: [],
   linker: {
-    ignore: ["フレル・ウィズ", "自由が丘", "東急"],
+    ignore: ["フレル・ウィズ", "自由が丘", "東急"]
   },
-};
-
-module.exports.scraper = [
-  ...[1, 2, 3, 4].map((level) => {
+  scraper: [1, 2, 3, 4].map(level => {
     return {
       url: `http://www.fullel.com/jiyugaoka/shopguide/?floor=${level}f`,
       venues: {
@@ -18,9 +15,11 @@ module.exports.scraper = [
           followLink: { selector: "a", property: "href" },
           name: { selector: ".shopCmnTitle01 dd" },
           phone: { selector: ".shopDetailInfo01 tr:nth-of-type(3) td" },
-          level,
-        },
-      },
+          level
+        }
+      }
     };
-  }),
-];
+  })
+};
+
+export = config;
