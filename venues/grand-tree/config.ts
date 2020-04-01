1e9018bacaa01d6574dfe3c26366cedb6f8f9a55
okitan <okitakunio@gmail.com>
2020-04-01 21:26:55 +0900

    :bulb: grand tree

---
+++ venues/grand-tree/config.ts
@@ -0,0 +1,27 @@
+import { Config } from "../../src/types/config";
+
+const config: Config = {
+  id: "5361cba0498e5839aa495469",
+  name: "グランツリー武蔵小杉",
+  subvenues: [{ id: "54a38f1b498ea176a18bd8ac", name: "グランツリー武蔵小杉-フードコート" }],
+  linker: {
+    ignore: ["グランツリー", "武蔵小杉", "ムサシコスギ"]
+  },
+  scraper: [
+    {
+      url: "http://www.grand-tree.jp/web/shop/index.html",
+      options: { waitUntil: "domcontentloaded" },
+      venues: {
+        "#shopList div.item:not(.all)": {
+          followLink: { selector: "a", property: "href" },
+          name: { selector: "h3", modifier: name => name.split("｜")[0] },
+          altName: { selector: "h3", modifier: name => name.split("｜")[1] },
+          phone: { xpath: "//dt[contains(text(), '電話')]/following-sibling::dd" }, // contains is only by xpath
+          level: { selector: "#subData dd:nth-of-type(1)", modifier: level => level.replace("F", "") }
+        }
+      }
+    }
+  ]
+};
+
+export = config;
