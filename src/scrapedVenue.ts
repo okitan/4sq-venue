ab308145fa18ac7acdc5c914179c06fcc56aee57
okitan <okitakunio@gmail.com>
2020-04-04 20:45:04 +0900

    :bulb: in and out is LTSVRecord

--- src/scrapedVenue.ts
+++ src/scrapedVenue.ts
@@ -1,6 +1,15 @@
-type Key = "name" | "altName" | "bldg" | "level" | "phone" | "url";
+import { LtsvRecord } from "ltsv/dist/cjs";
 
-export class ScrapedVenue {
+type Properties = {
+  name: string;
+  altName?: string;
+  bldg?: string;
+  level?: number;
+  phone?: string;
+  url?: string;
+};
+
+export class ScrapedVenue implements Properties {
   name: string;
   altName?: string;
   bldg?: string;
@@ -8,7 +17,7 @@ export class ScrapedVenue {
   phone?: string;
   url?: string;
 
-  constructor({ name, altName, bldg, level, phone, url }: Pick<ScrapedVenue, Key>) {
+  constructor({ name, altName, bldg, level, phone, url }: Properties) {
     this.name = name;
     this.altName = altName;
     this.bldg = bldg;
@@ -17,27 +26,24 @@ export class ScrapedVenue {
     this.url = url;
   }
 
-  static keys(): Key[] {
+  static keys(): (keyof Properties)[] {
     return ["name", "altName", "bldg", "level", "phone", "url"];
   }
 
   // TOOD: FIXME: return undefined instead of object
-  static create(obj: { [x: string]: ScrapedVenue[Key] }): ScrapedVenue | {} {
+  static parse(obj: LtsvRecord): ScrapedVenue | {} {
     if (!obj) return {};
 
     const venue = this.keys().reduce((result, e) => {
       // because `""` is falsy in javascript `hoge:` is converted to `{ hoge: undefined }`
-      // Note: `0` is also converted to undefined, but I just have `level` as number and there are no level:0
-      if (obj[e]) {
+      // Note: `0` is also converted to undefined, but I only have `level` as number and there are no level:0 in my world
+      if (obj[`scraped.${e}`]) {
+        const value: string = obj[`scraped.${e}`];
         // @ts-ignore result[e] = does not work
-        result[e] = obj[e];
-      } else if (obj[`scraped.${e}`]) {
-        // @ts-ignore result[e] = does not work
-        result[e] = obj[`scraped.${e}`];
+        result[e] = typeof result[e] === "number" ? parseInt(value) : value;
       }
-
       return result;
-    }, {} as { [x in Key]: ScrapedVenue[x] });
+    }, {} as Properties);
 
     // FIXME: do not do like this
     if (Object.keys(venue).length === 0) return {};
@@ -45,14 +51,14 @@ export class ScrapedVenue {
     return new ScrapedVenue(venue);
   }
 
-  format(cascade: boolean = false): { [x: string]: Exclude<ScrapedVenue[Key], undefined> } {
+  format(cascade: boolean = false): LtsvRecord {
     return ScrapedVenue.keys().reduce((result, key) => {
       if (cascade) {
-        result[`scraped.${key}`] = this[key] ?? "";
+        result[`scraped.${key}`] = this[key]?.toString() ?? "";
       } else {
-        result[key] = this[key] ?? "";
+        result[key] = this[key]?.toString() ?? "";
       }
       return result;
-    }, {} as { [x: string]: Exclude<ScrapedVenue[Key], undefined> });
+    }, {} as LtsvRecord);
   }
 }
