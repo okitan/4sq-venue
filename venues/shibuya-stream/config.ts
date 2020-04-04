621750632dc729d87eee388172e7cf1224b47412
okitan <okitakunio@gmail.com>
2020-04-04 16:12:29 +0900

    :bulb: forgot to add

---
+++ venues/shibuya-stream/config.ts
@@ -0,0 +1,34 @@
+import { Config } from "../../src/types/config";
+
+const config: Config = {
+  id: "597f0333dec1d60e99087df2",
+  name: "渋谷ストリーム",
+  subvenues: [
+    {
+      id: "5c0b73c0dee770002c73c9da",
+      name: "TORQUE Spice & Herb, Table & Court",
+    },
+  ],
+  linker: {
+    ignore: ["渋谷", "ストリーム"],
+  },
+  scraper: [
+    {
+      url: "https://shibuyastream.jp/shop/",
+      venues: {
+        "#shop_lists li": {
+          followLink: { selector: "a", property: "href" },
+          name: { selector: ".shop_name" },
+          altName: { selector: ".shop_kana" },
+          phone: { selector: ".shop_detail_list dd:nth-of-type(3)" },
+          level: {
+            selector: ".shop_detail_list .floor_no",
+            modifier: (e) => parseInt(e.replace("F", "")),
+          },
+        },
+      },
+    },
+  ],
+};
+
+export = config;
