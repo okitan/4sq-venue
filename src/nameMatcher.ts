dd4362136e7a272b2263ef0c70940edf99f0d18e
okitan <okitakunio@gmail.com>
2020-04-05 12:19:41 +0900

    :bulb: nameMatcher

--- src/nameMatcher.ts
+++ src/nameMatcher.ts
@@ -1,8 +1,6 @@
-"use strict";
+import mdiff from "mdiff";
 
-const mdiff = require("mdiff").default;
-
-const isEqualName = (a, b, { ignore = [] } = {}) => {
+export function isEqualName(a?: string, b?: string, { ignore = [] }: { ignore?: string[] } = {}) {
   if (!a || !b || a.length === 0 || b.length === 0) return false;
 
   const regexp = new RegExp(ignore.join("|"), "g");
@@ -15,10 +13,10 @@ const isEqualName = (a, b, { ignore = [] } = {}) => {
   if (aName.length === 0 || bName.length === 0) return a.toLowerCase() === b.toLowerCase();
 
   return aName === bName;
-};
+}
 
 // more than 5 characters matched is required
-const getSimilarity = (a, b, { ignore = [] } = {}) => {
+export function getSimilarity(a?: string, b?: string, { ignore = [] }: { ignore?: string[] } = {}) {
   if (!a || !b || a.length === 0 || b.length === 0) return 0;
 
   const regexp = new RegExp(ignore.join("|"), "g");
@@ -30,11 +28,7 @@ const getSimilarity = (a, b, { ignore = [] } = {}) => {
 
   const minLength = Math.min(aName.length, bName.length, 5);
   const lcs = mdiff(aName, bName).getLcs();
+  if (!lcs) return 0;
 
   return lcs.length >= minLength ? Math.max(lcs.length / aName.length, lcs.length / bName.length) : 0;
-};
-
-module.exports = {
-  isEqualName,
-  getSimilarity,
-};
+}
