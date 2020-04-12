bbc34eeede565142114997a4938d1759d8b3e6ed
okitan <okitakunio@gmail.com>
2020-04-12 22:52:07 +0900

    :bulb: generate config in ts

---
+++ src/commands/add.ts
@@ -0,0 +1,78 @@
+import yargs from "yargs";
+import yeoman from "yeoman-environment";
+import Generator from "yeoman-generator";
+
+import { Extract } from "../cli";
+import { addFoursquareClientOptions, FoursquareClient } from "../services/4sq";
+import { Venue } from "../venue";
+
+export const command = "add <target>";
+export const description = "add venue";
+
+export function builder(yargs: yargs.Argv) {
+  return addFoursquareClientOptions(yargs)
+    .positional("target", { type: "string", demandOption: true })
+    .options({
+      venueId: {
+        type: "string",
+        desc: "foursquare parent venue id",
+        alias: "venue",
+        required: true,
+      },
+      subVenueId: {
+        type: "string",
+        array: true,
+        desc: "foursquare parnet subvenue id",
+        alias: "subVenues",
+        default: [],
+      },
+    });
+}
+
+export function handler(args: yargs.Arguments<Extract<ReturnType<typeof builder>>>) {
+  const env = yeoman.createEnv();
+
+  // @ts-ignore Generator should be typed as class
+  const generator = env.instantiate(VenueGenerator, { arguments: [], options: args });
+
+  generator.run();
+}
+
+export class VenueGenerator extends Generator {
+  venue?: Venue;
+  subVenues: Venue[] = [];
+
+  get foursquareClient(): FoursquareClient {
+    return this.options.foursquareClient;
+  }
+
+  async initializing() {
+    const options = this.options as yargs.Arguments<Extract<ReturnType<typeof builder>>>;
+
+    // fetch venue
+    this.venue = new Venue(await (await this.foursquareClient.getVenue({ venueId: options.venueId })).venue);
+    // fetch subvenues
+    this.subVenues = await Promise.all(
+      options.subVenueId.map(async (id) => new Venue((await this.foursquareClient.getVenue({ venueId: id })).venue))
+    );
+  }
+
+  writing() {
+    if (!this.venue) throw `no venue assigned`; //never
+
+    const options = this.options as yargs.Arguments<Extract<ReturnType<typeof builder>>>;
+    const target = options.target;
+
+    // config.js
+    this.fs.copyTpl(this.templatePath("config.ts"), this.destinationPath(`venues/${target}/config.ts`), {
+      id: this.venue.id,
+      name: this.venue.name,
+      subVenues: this.subVenues.map((venue) => {
+        return { id: venue.id, name: venue.name };
+      }),
+    });
+    this.spawnCommand("npx", ["prettier", "--write", this.destinationPath(`venues/${target}/config.ts`)]);
+
+    // github actions
+  }
+}
