79df8bce3de9aa035952b6a7ef4f4cf803dff36b
okitan <okitakunio@gmail.com>
2020-04-12 21:24:49 +0900

    :bulb: also remove

---
+++ src/commands/link/remove.ts
@@ -0,0 +1,46 @@
+import yargs from "yargs";
+
+import { Extract } from "../../cli";
+import { guessTargetFromBranchName } from "../../util";
+import { Venue } from "../../venue";
+import { loadLinkedVenues, loadNotLinkedVenues, updateLinkedVenues, updateNotLinkedVenues } from "../../venueList";
+
+export const command = "remove";
+export const description = "remove link";
+
+export function builder(yargs: yargs.Argv) {
+  return yargs.options({
+    target: {
+      type: "string",
+      default: guessTargetFromBranchName(),
+      demandOption: true,
+    },
+    name: {
+      type: "string",
+      array: true,
+      demandOption: true,
+    },
+  });
+}
+
+export function handler({ target, name: names }: yargs.Arguments<Extract<ReturnType<typeof builder>>>) {
+  if (!target) throw `target should not be null`; // never
+
+  const linkedVenues = loadLinkedVenues(target);
+  const notLinkedVenues = loadNotLinkedVenues(target);
+
+  // console.log(notLinkedVenues);
+  names.forEach((name) => {
+    const linkedVenue = linkedVenues.removeVenue(new Venue({ name }), { guess: true });
+
+    if (!linkedVenue || !linkedVenue.scraped) throw `no venue found for ${name}`;
+
+    const scrapedVenue = Venue.fromScraped(linkedVenue.scraped);
+    linkedVenue.scraped = undefined;
+
+    notLinkedVenues.push(scrapedVenue, linkedVenue);
+  });
+
+  updateLinkedVenues(target, linkedVenues);
+  updateNotLinkedVenues(target, notLinkedVenues);
+}
