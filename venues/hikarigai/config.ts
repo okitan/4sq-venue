24dedc9cbbeb0270ee5c48f5d1ffaa5b007212ce
okitan <okitakunio@gmail.com>
2021-01-10 15:09:47 +0900

    :wrench: omit ()

--- venues/hikarigai/config.ts
+++ venues/hikarigai/config.ts
@@ -5,7 +5,7 @@ const config: Config = {
   name: "自由が丘 ひかり街",
   subvenues: [],
   linker: {
-    ignore: [],
+    ignore: ["自由が丘", "ひかり街"],
   },
   scraper: [
     {
@@ -27,12 +27,12 @@ const config: Config = {
         },
         // 1, 2 is missing 3 is LV1 4 is LV2 5 is LV3
         "article#post-83 > .entry-content > div:nth-child(4) li": {
-          name: { xpath: "." },
+          name: { xpath: ".", modifier: (txt) => txt.split("（")[0] },
           phone: { xpath: "./following-sibling::p[1]" },
           level: 2,
         },
         "article#post-83 > .entry-content > div:nth-child(5) li": {
-          name: { xpath: "." },
+          name: { xpath: ".", modifier: (txt) => txt.split("（")[0] },
           phone: { xpath: "./following-sibling::p[1]" },
           level: 3,
         },
